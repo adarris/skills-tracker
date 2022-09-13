88bce3f2bae77f596251e28f2367f92cf6fe84df
adrianadarris <adrianadarris@gmail.com>
2022-09-12 22:27:31 -0500

    added bonuses

--- counting-characters-studio/src/Main.java
+++ counting-characters-studio/src/Main.java
@@ -1,10 +1,14 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String quote = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
+        System.out.println("Give me a string to count characters: ");
+        Scanner input = new Scanner(System.in);
+        String quote = input.nextLine().toLowerCase();
+        //String quote = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
         HashMap<Character, Integer> characters = new HashMap<>();
 
         char charactersFromQuote[] = quote.toCharArray();
